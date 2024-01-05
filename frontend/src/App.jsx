9cfad3548de6c2880a39021999a7f0088a13994a
Claire Vannier <clairevannierpro@gmail.com>
2024-01-05 15:56:37 +0100

    finish connexion component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./components/landing-page/Home";
+import Login from "./components/login/Login";
+import RegisterParent from "./components/register/RegisterParent";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<RegisterParent />} />
       </Routes>
     </div>
   );
---
+++ frontend/src/components/login/Login.jsx
@@ -0,0 +1,75 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import logobaby from "../../assets/logobaby.svg";
+import logocoeur from "../../assets/logocoeur.svg";
+
+function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Ajoutez ici la logique pour traiter les données du formulaire (connexion, appel à l'API, etc.)
+    console.info("Données du formulaire soumises :", formData);
+    // Réinitialiser le formulaire après la soumission si nécessaire
+    setFormData({
+      email: "",
+      password: "",
+    });
+  };
+
+  return (
+    <section className="formContainer">
+      <div className="logoForm">
+        <img id="logoFormCoeur" src={logocoeur} alt="coeur du site" />
+        <img id="logoFormTxt" src={logobaby} alt="texte du site" />
+      </div>
+      <div className="loginForm">
+        <h2 className="titleForm">Je me connecte</h2>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              placeholder="Email"
+              required
+            />
+          </label>
+          <label>
+            Password:
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              placeholder="Password"
+              required
+            />
+          </label>
+          <button className="formBtn" type="submit">
+            Se connecter
+          </button>
+          <p>
+            Nouveau sur BabyPlace ?{" "}
+            <Link to="/register">Inscrivez-vous ici</Link>
+          </p>
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default Login;
